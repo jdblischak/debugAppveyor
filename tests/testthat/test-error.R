ddfbf3ad979e67d603b94b62a85d35cd0c14e8af
John Blischak <jdblischak@gmail.com>
2018-10-19 15:48:41 -0400

    Try to determine exact cause of dir.create error.

--- tests/testthat/test-error.R
+++ tests/testthat/test-error.R
@@ -1,32 +1,45 @@
 context("test-error.R")
 
-test_that("1984", {
-  expect_equal(2 + 2, 5)
+test_that("Can create directory with absolute path", {
+  d <- fs::path_norm(tempfile())
+  dir.create(d)
+  expect_true(dir.exists(d))
 })
 
-test_that("rename works", {
-  d1 <- tempfile()
-  d2 <- tempfile()
-  dir.create(d1)
-  file.create(file.path(d1, "file.txt"))
-  d1_rel <- fs::path_rel(d1)
-  d2_rel <- fs::path_rel(d2)
-  file.rename(d1_rel, d2_rel)
-  expect_true(dir.exists(d2_rel))
-  expect_false(dir.exists(d1_rel))
+test_that("Can create directory with relative path", {
+  d <- fs::path_rel(tempfile())
+  dir.create(d)
+  expect_true(dir.exists(d))
 })
 
-test_that("wflow_rename works", {
-  library(workflowr)
-  start <- wflow_start(tempfile(), git = FALSE, change_wd = FALSE)
-  tdir <- workflowr:::relative(start$directory)
-  original <- file.path(tdir, "code")
-  new <- file.path(tdir, "scripts")
-  renamed <- wflow_rename(original, new, project = tdir)
-  expect_false(dir.exists(original))
-  expect_true(dir.exists(new))
-  # browser()
+test_that("Can create directory with absolute path with for loop", {
+  d <- fs::path_norm(c(tempfile(), tempfile(), tempfile()))
+  for (i in seq_along(d)) {
+    dir.create(d[i])
+  }
+  expect_true(all(dir.exists(d)))
 })
+
+test_that("Can create directory with relative path with for loop", {
+  d <- fs::path_rel(c(tempfile(), tempfile(), tempfile()))
+  for (i in seq_along(d)) {
+    dir.create(d[i])
+  }
+  expect_true(all(dir.exists(d)))
+})
+
+test_that("Can create directory with absolute path with lapply", {
+  d <- fs::path_norm(c(tempfile(), tempfile(), tempfile()))
+  lapply(d, function(x) dir.create(x))
+  expect_true(all(dir.exists(d)))
+})
+
+test_that("Can create directory with relative path with lapply", {
+  d <- fs::path_rel(c(tempfile(), tempfile(), tempfile()))
+  lapply(d, function(x) dir.create(x))
+  expect_true(all(dir.exists(d)))
+})
+
 
 test_that("Can create directories", {
   newfile <- workflowr:::absolute(tempfile("a/"))
