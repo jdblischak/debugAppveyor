7803f9977c1fa83759b52cc8848486706bf82d29
John Blischak <jdblischak@gmail.com>
2018-10-19 13:40:49 -0400

    Use Notepad++ as text editor.

--- scripts/setup.R
+++ scripts/setup.R
@@ -1,1 +1,4 @@
-.libPaths("C:\\RLibrary")
+# Access the installed dependencies
+.libPaths("C:/RLibrary")
+# Use Notepad++ to edit files with file.edit()
+options(editor = "C:/Program Files (x86)/Notepad++/notepad++.exe")
