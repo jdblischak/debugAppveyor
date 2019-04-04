e90dbac6f3e892944a259b53f5923eedf7a3341e
John Blischak <jdblischak@gmail.com>
2019-04-04 16:43:41 -0400

    Don't start debugger at beginning of build.

--- scripts/setup.R
+++ scripts/setup.R
@@ -1,5 +1,6 @@
 # Access the installed dependencies
 .libPaths("C:/RLibrary")
+
 # Use Notepad++ to edit files with file.edit()
 options(editor = "C:/Program Files (x86)/Notepad++/notepad++.exe")
 
--- scripts/setup.ps1
+++ scripts/setup.ps1
@@ -6,10 +6,14 @@ Import-Module "..\appveyor-tool.ps1"
 
 Progress "appveyor-tool module loaded"
 
+# Add R.exe and Rscript.exe to path
 $env:Path = "C:\R\bin;" + $env:Path
 
+# Set user library
 $env:R_LIBS_USER = "C:\Rlibrary"
 
+# Confirm library paths
 Rscript.exe -e ".libPaths()"
 
+# Run tests with the travis-tool
 .\travis-tool.sh.cmd run_tests
